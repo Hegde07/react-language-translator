170bb730792d25b37e4e6516e80faad0c1a5739a
Hegde07 <hegdebhargav92@gmail.com>
2023-09-03 22:10:49 -0700

    Added translate Api

--- src/App.js
+++ src/App.js
@@ -4,46 +4,59 @@ import './App.css';
 import axios from 'axios';
 
 function App() {
-  const [to,setTo]=useState('');
-  const [from,setFrom]=useState('')
+  const [options,setOptions]=useState([])
+  const [to,setTo]=useState('en');
+  const [from,setFrom]=useState('en')
   const [input,setInput]=useState('')
   const [output,setOutput]=useState('');
+
+ 
+  const handleTranslate=()=>{
+      const params = new URLSearchParams();
+      params.append('q',input);
+      params.append('source',from);
+      params.append('target',to);
+      // params.append('api_key','xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx')
+      axios.post('https://libretranslate.com/translate',params,
+      {
+        headers:{'accept':'application/json','Content-Type':'application/x-www-form-urlencoded'}
+      }).then(res=> setOutput(res.data.transatedText)
+     )
+  }
 
 
   useEffect(()=>{
     axios.get('https://libretranslate.com/languages',{
       headers:{'accept':'application/json'}
-    }).then(res=>console.log(res))
+    }).then(res=>setOptions(res.data))
+    
   },[])
 
   return (
     <div className="App">
       <div className='selector'>
-      From:
-     <select>
-      <option value='1'>1</option>
-      <option value='2'>2</option>
-      <option value='3'>3</option>
-      <option value='4'>4</option>
-      <option value='5'>5</option>
+      From ({from}):
+     <select onChange={e=>setFrom(e.target.value)}>
+       {options.map((option)=>(
+         <option key={option.code} value={option.code}>{option.name}</option>
+       ))}
      </select>
-     To:
-     <select>
-      <option value='1'>1</option>
-      <option value='2'>2</option>
-      <option value='3'>3</option>
-      <option value='4'>4</option>
-      <option value='5'>5</option>
+     {" "}
+     To ({to}):
+     <select onChange={e=>setTo(e.target.value)}>
+       {options.map((option,index)=>(
+         <option key={option.code} value={option.code}>{option.name}</option>
+       ))}
      </select>
      </div>
      <div className='textarea1'>
-      <textarea cols='50' rows='8'></textarea>
+      <textarea cols='50' rows='8' onInput={e=>setInput(e.target.value)}></textarea>
      </div>
      <div className='textarea2'>
-      <textarea cols='50' rows='8'></textarea>
+      <textarea cols='50' rows='8' value={output}></textarea>
      </div>
      <div>
-      <button>Translate</button>
+      <button onClick={e=>handleTranslate()}>Translate</button>
      </div>
     </div>
   );
